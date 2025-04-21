e69c1a284d7ad3d88f8840cf621d2e143702d025
Zowie Erickson <zowie.erickson@gmail.com>
2025-04-21 14:43:56 -0400

    Add app icon and font selector to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,24 @@
+import AppIcon from '../../assets/images/logo.svg?react'
+import ArrowDownIcon from '../../assets/images/icon-arrow-down.svg?react'
+import styles from './Navbar.module.css'
 
 
 export default function Navbar() {
     return (
-        <nav>
-            <h1>This is the Navbar</h1>
+        <nav className={styles.nav}>
+            <a href="/"><AppIcon/></a>
+            <div className={styles.controls}>
+                <button className={styles.fontSelector}>
+                    Sans Serif
+                    <ArrowDownIcon/>
+                </button>
+                <ul className={styles.fontsList}>
+                    <li>Sans Serif</li>
+                    <li>Serif</li>
+                    <li>Mono</li>
+                </ul>
+                <div className='vertical-divider'></div>
+            </div>
         </nav>
     )
 }
