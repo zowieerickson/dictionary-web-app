452c9180e01dc26d5712fcbdc60757c736689ad7
Zowie Erickson <zowie.erickson@gmail.com>
2025-04-21 22:40:41 -0400

    feat(dropdown): implement dropdown toggling when clicking outside the dropdown

--- src/components/FontDropdownSelector/FontDropdownSelector.jsx
+++ src/components/FontDropdownSelector/FontDropdownSelector.jsx
@@ -1,19 +1,46 @@
-import { useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import ArrowDownIcon from '../../assets/images/icon-arrow-down.svg?react'
 import styles from './FontDropdownSelector.module.css'
 
 export default function FontDropdownSelector() {
     const [dropdownOpen, setDropdownOpen] = useState(false)
+    const fontDropdownRef = useRef(null)
+    const fontBtnRef = useRef(null)
+    const dropdownOpenRef = useRef(dropdownOpen)
 
-    console.log(dropdownOpen)
+    useEffect(() => {
+        dropdownOpenRef.current = dropdownOpen
+    }, [dropdownOpen])
+
+    useEffect(() => {
+        function handleDocumentClick(e) {
+            console.log(e.target, dropdownOpen)
+            if (
+                !fontBtnRef.current.contains(e.target) &&
+                !fontDropdownRef.current.contains(e.target) &&
+                dropdownOpenRef.current === true
+            ) {
+                setDropdownOpen(false)
+            }
+        }
+
+        document.addEventListener("click", handleDocumentClick)
+
+        return () => {
+            document.removeEventListener("click", handleDocumentClick)
+        }
+    }, [])
 
     return (
         <>
-            <button onClick={() => setDropdownOpen(prev => !prev)} className={styles.fontSelector} >
+            <button onClick={() => setDropdownOpen(prev => !prev)} ref={fontBtnRef} className={styles.fontSelector} >
                 Sans Serif
                 <ArrowDownIcon/>
             </button>
-            <ul className={`${styles.fontsList} ${dropdownOpen ? 'visible' : ''}`}>
+            <ul 
+                className={`${styles.fontsList} ${dropdownOpen ? 'visible' : ''}`}
+                ref={fontDropdownRef}
+            >
                 <li>Sans Serif</li>
                 <li>Serif</li>
                 <li>Mono</li>
