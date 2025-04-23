2ae4782c44afec336524bdde597ab003483a09d2
Zowie Erickson <zowie.erickson@gmail.com>
2025-04-22 23:16:33 -0400

    feat(ui): add moon icon to nav bar per Figma design

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
-import AppIcon from '../../assets/images/logo.svg?react'
+import IconApp from '../../assets/images/logo.svg?react'
+import IconMoon from '../../assets/images/icon-moon.svg?react'
 import styles from './Navbar.module.css'
 import FontDropdownSelector from '../FontDropdownSelector/FontDropdownSelector.jsx'
 import ThemeToggle from '../ThemeToggle/ThemeToggle.jsx'
@@ -7,11 +8,12 @@ import ThemeToggle from '../ThemeToggle/ThemeToggle.jsx'
 export default function Navbar() {
     return (
         <nav className={styles.nav}>
-            <a href="/"><AppIcon/></a>
+            <a href="/"><IconApp/></a>
             <div className={styles.controls}>
                 <FontDropdownSelector/>
                 <div className='vertical-divider'></div>
                 <ThemeToggle />
+                <IconMoon />
             </div>
         </nav>
     )
