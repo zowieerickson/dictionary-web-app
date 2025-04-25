9a048e148da7c79d01effc42ccff562e712dd498
Zowie Erickson <zowie.erickson@gmail.com>
2025-04-24 22:35:15 -0400

    feature: implemented an audio file to be fetched for every search

--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -5,13 +5,21 @@ import styles from './SearchResults.module.css'
 
 export default function SearchResults({ definition }) {
     const [meanings, setMeanings] = useState('');
+    const [audioFile, setAudioFile] = useState('');
+
+    useEffect(() => {
+        if (definition) {
+            const firstAudio = definition[0].phonetics.find(p => p.audio)
+            setAudioFile(firstAudio?.audio || null)
+        }
+    }, [definition])
 
     if(!definition) return null
 
-    const mp3Url = "https://api.dictionaryapi.dev/media/pronunciations/en/keyboard-us.mp3"; // Keyboard audio
+    const mp3UrlTest = audioFile
 
     const handlePlay = () => {
-        const audio = new Audio(mp3Url)
+        const audio = new Audio(mp3UrlTest)
         audio.play().catch((error) => {
             console.error('Error playing audio: ', error)
         })
@@ -27,8 +35,15 @@ export default function SearchResults({ definition }) {
                 </div>
                 <button onClick={handlePlay}><IconPlay /></button>
             </header>
-            <article>
-                <h3 className="label-line">{definition[0].meanings[0].partOfSpeech}</h3>
+            <article className={styles.meaning}>
+                <h3 className="label-line"><i>{definition[0].meanings[0].partOfSpeech}</i></h3>
+                <h4>Meaning</h4>
+                <ul>
+                    {definition[0].meanings[0].definitions.map(definition1 => {
+                        return <li>{definition1.definition}</li>
+                    })}
+                </ul>
+                
             </article>
             <p>{definition[0].meanings[0].definitions[0].definition}</p>
             {/* <button onClick={() => handlePlay}>{definition[0].phonetics[2].audio}</button> */}
