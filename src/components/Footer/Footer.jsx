86ec12c1e02daaf70446d35727ebe0a188d9de00
Zowie Erickson <zowie.erickson@gmail.com>
2025-04-26 21:30:55 -0400

    style: update styles for footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,22 @@
 import styles from './Footer.module.css'
+import IconNewWindow from '../../assets/images/icon-new-window.svg?react'
 
 export default function Footer({ data }) {
     if (!data) return
 
 
     return (
+        <>
         <footer className={styles.footer}>
-            <h4>Source</h4>
-            <p><a href={data[0].sourceUrls[0]} target="_blank" rel="noopener noreferrer">{data[0].sourceUrls[0]}</a></p>
+            <hr className={styles.footerLine}/>
+            <div className={styles.source}>
+                <h4 className={styles.sourceTitle}>Source</h4>
+                <a className={styles.sourceLink} href={data[0].sourceUrls[0]} target="_blank" rel="noopener noreferrer">
+                    {data[0].sourceUrls[0]}
+                    <IconNewWindow />
+                </a>
+            </div>
         </footer>
+        </>
     )
 }
