b198e50d41e1e144ba0121c6fa91aefd3956b5ae
Zowie Erickson <zowie.erickson@gmail.com>
2025-04-27 21:06:33 -0400

    feature: show error messge if searching for an empty word

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -7,13 +7,14 @@ import NoResultsMessage from '../NoResultsMessage/NoResultsMessage';
 
 export default function Search({ searchWord, setSearchWord, setData, setError }) {
     const [loading, setLoading] = useState(false);
+    const [hasSearched, setHasSearched] = useState(false)
 
     const handleSearch = async(e) => {
         e.preventDefault()
         setLoading(true) // Start loading
         setError(null) // Reset error state on new search
         setData(null) // Reset previous data
-
+        setHasSearched(true)
         try {
             const result = await fetchData(searchWord)
             setData(result)
@@ -23,19 +24,35 @@ export default function Search({ searchWord, setSearchWord, setData, setError })
         } finally {
             setLoading(false)
         }
+
     }
+    console.log(  searchWord )
 
     const handleChange = function(e) {
         setSearchWord(e.target.value)
     }
+    
+    searchWord ? console.log('true word') : console.log(' false word')
 
 
     return (
         <>
-            <form action="" onSubmit={handleSearch} className={styles.searchForm}>
-                <input placeholder="Search for any word..." onChange={handleChange} value={searchWord} className={styles.search} type="search" name="" id="" />
+            <form 
+                action=""
+                onSubmit={handleSearch}
+                className={styles.searchForm}
+            >
+                <input 
+                    placeholder="Search for any word..."
+                    onChange={handleChange}
+                    value={searchWord}
+                    className={`${styles.search} ${!searchWord && hasSearched ? styles.errorBorder : ''}`}type="search"
+                    name=""
+                    id=""
+                />
                 <button className={styles.searchBtn} type="submit" aria-label='Search'><SearchIcon className={styles.searchIcon}/></button>
             </form>
+            {!searchWord && hasSearched && (<p role="alert" className={styles.errorMessage}>Whoops, can't be empty...</p>)}
         </>
     )
 }
